276e5eaefe60f0c539d30e06f668052e2592fa57
FrapoDeveloper <frapo_developer@outlook.com.pe>
2022-09-07 23:20:50 -0500

    Desing and libraries of the Kotflix

--- app/src/main/java/com/fraporitmostech/kotflix/MainActivity.kt
+++ app/src/main/java/com/fraporitmostech/kotflix/MainActivity.kt
@@ -6,6 +6,5 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
     }
 }
